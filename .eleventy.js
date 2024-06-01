fe2868e533e5092e9cafd5f84cecec3822c2ac3b
Stefan Heinemann <stefan.heinemann@codedump.ch>
2024-06-01 12:22:35 +0200

    Add some basic style to the blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,13 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
+
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/@fontsource/playfair-display/files/playfair-display-latin-400-normal.woff":
+      "fonts/playfair-display-latin-400-normal.woff",
+  });
+
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/@fontsource/urbanist/files/urbanist-latin-400-normal.woff":
+      "fonts/urbanist-latin-400-normal.woff",
+  });
 };
