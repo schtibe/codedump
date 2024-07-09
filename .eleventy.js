1f0d8486d64db8b5ba94d4db8a387a7e9715b15b
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2024-07-09 17:24:43 +0200

    Format the date correctly

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toISODate()
+  });
+
   eleventyConfig.addPassthroughCopy("bundle.css");
 
   eleventyConfig.addPassthroughCopy({
